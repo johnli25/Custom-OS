b789662bff4cdad071f53bc3348ae2a512fedb5f
John Li <warriorjohn36@gmail.com>
2022-03-14 13:06:11 -0500

    more work on pag

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,4 +1,29 @@
 #include "paging.h" //include header files
 
+void setup_paging_structures(){
+    int i;
+    for (i = 0; i < TOTAL_ENTRIES; i++){
+        
+    }
+}
 
+void initialize_paging(){
+
+
+    // asm volatile(
+    //     "movl $page_directory, %eax ;"
+    //     /*andl $0xfffffc00, %eax*/
+    //     "movl %eax, %cr3            ;"
+
+    //     /*set the paging (PG) and protection (PE) bits of CR0.*/
+    //     "movl %cr4, %eax        ;"
+    //     "orl $0x10, %eax        ;"
+    //     "movl %eax, %cr4        ;"
+
+    //     /*enable PSE (4 MiB pages)*/
+    //     "movl %cr0, %eax        ;"
+    //     "orl $0x80000001, %eax  ;"
+    //     "movl %eax, %cr0        ;"
+    // );
+}
 
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,5 +1,20 @@
 #ifndef _PAGING_H
 #define _PAGING_H
 
+#include "types.h"
 
+#define TOTAL_ENTRIES 1024
+
+//create 1024 (use only first 2-ignore next 1022 entries) PDs entries, each 4 MB each
+//each PD is either gonna point to 1) 1 page (4 MB) or 2) another page TABLE (1024 Entries, each PT is 4KB)
+//each PT entry = page, will point to a 4 KB page (each page is 4 KB)
+
+typedef struct PDE_kernel_4MB{
+
+} PDE_kernel_4MB;
+
+extern void setup_paging_structures();
+extern void initialize_paging();
+
+#endif
 
