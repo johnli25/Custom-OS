d17892f05a04eb83bb0cf9439efc73f764921dac
dhyey2 <dhyey2@illinois.edu>
2022-03-15 11:58:01 -0500

    updated enable_irq and disable_irq

--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -12,7 +12,9 @@
 /* Ports that each PIC sits on */
 #define MASTER_8259_PORT    0x20
 #define SLAVE_8259_PORT     0xA0
-
+/* Adding Data Ports */
+#define MASTER_DATA    (MASTER_8259_PORT+1)
+#define SLAVE_DATA     (SLAVE_8259_PORT+1)
 /* Initialization control words to init each PIC.
  * See the Intel manuals for details on the meaning
  * of each word */
