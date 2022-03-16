d55bb2bec7716e9d961328ae232ec61413b06f01
dhyey2 <dhyey2@illinois.edu>
2022-03-16 12:35:24 -0500

    updated pic init

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -6,12 +6,40 @@
 #include "lib.h"
 
 /* Interrupt masks to determine which interrupts are enabled and disabled */
-uint8_t master_mask; /* IRQs 0-7  */
-uint8_t slave_mask;  /* IRQs 8-15 */
+uint8_t master_mask = 0xff; /* IRQs 0-7  */
+uint8_t slave_mask = 0xff;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-    //auto_eoi is 0?
+    //auto_eoi is 0
+    unsigned long flags;
+    unsigned char a1, a2;
+    a1 = inb(MASTER_DATA);              
+	a2 = inb(SLAVE_DATA);
+
+    spin_lock_irqsave(&i8259_lock, flags) //spin lock
+
+    outb(master_mask, MASTER_DATA);//masking our data ports
+    outb(slave_mask, SLAVE_DATA);
+    
+    outb_p(ICW1, MASTER_8259_PORT);
+    outb_p(MASTER_8259_PORT+0, MASTER_DATA);
+    outb_p(ICW3_MASTER, MASTER_DATA);
+    
+    outb_p(ICW4, MASTER_DATA);
+
+    outb_p(ICW1, SLAVE_8259_PORT);
+    outb_p(ICW2_MASTER + PICCheck, SLAVE_DATA);
+    outb_p(ICW3_SLAVE, SLAVE_DATA);
+    outb_p(ICW4, SLAVE_DATA);
+    
+    udelay(100);
+    //now restore masks with a1, a2
+    outb(MASTER_DATA, a1);
+	outb(SLAVE_DATA, a2);
+
+    spin_unlock_irqrestore(&i8259_lock, flags);
+
     
 }
 
