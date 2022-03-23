8bda928a2d55cbd2b5a7eea78669c166aeae609e
dhyey2 <dhyey2@illinois.edu>
2022-03-23 17:48:49 -0500

    added scancodesCap and scancodesCapShift

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -5,3 +5,38 @@ extern void initialize_Keyboard(void);
 //Used as the Handler for the Keyboard
 extern void interrupt_keyboard(void);
 
+//scancodesCap array - converts the character to the correct character when cap is on
+/* 
+ * scancodesCap array
+ *   DESCRIPTION: Used to convert the input data into a corresponding
+ *          character - based off of scancodes set cap from OSDev
+
+ */
+uint8_t scancodesCapLetters[scancodesSize] = {
+    ' ', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', ' ',
+    ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+    ' ', ' ', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`',
+    ' ', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+    '3', '0', '.'
+
+};
+
+//scancodesCapShift array - converts the character to the correct character when cap and shift is on
+/* 
+ * scancodesCapShift array
+ *   DESCRIPTION: Used to convert the input data into a corresponding
+ *          character - based off of scancodes set cap and shift from OSDev
+
+ */
+uint8_t scancodesCapShift[scancodesSize] = {
+    ' ', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', ' ',
+    ' ', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}',
+    ' ', ' ', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '\"', '~',
+    ' ', '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '\?', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+    '3', '0', '.'
+
+};
