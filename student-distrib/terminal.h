ad9549e94c60ddac6cb7cef0f8e741cd65973fd8
dhyey2 <dhyey2@illinois.edu>
2022-03-23 15:51:03 -0500

    Updating function parameters

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -2,6 +2,6 @@ extern int terminal_open(void);
 
 extern int terminal_close(void);
 
-extern int terminal_read(void);
+extern int terminal_read(int n, unsigned char * buf); //will add fd chkpt3
 
-extern int terminal_write(void);
+extern int terminal_write(int n, unsigned char * buf); //will add fd chkpt3
