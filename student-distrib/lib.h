7e15490157569d529de19c87fa348403e3f72c6d
vrajesh2 <vrajesh2@illinois.edu>
2022-03-27 16:41:40 -0500

    Update lib.h

--- student-distrib/lib.h
+++ student-distrib/lib.h
@@ -7,15 +7,27 @@
 
 #include "types.h"
 
+
 int32_t printf(int8_t *format, ...);
 void putc(uint8_t c);
+void putBackspace(uint8_t c);
 int32_t puts(int8_t *s);
 int8_t *itoa(uint32_t value, int8_t* buf, int32_t radix);
 int8_t *strrev(int8_t* s);
 uint32_t strlen(const int8_t* s);
 void clear(void);
-
+void clearText(void);
+void newLine(void);
+void verticalScroll(void);
 void test_interrupts(void);
+void clearBottom(void);
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
+void disable_cursor();
+void update_cursor(int x, int y);
+uint16_t get_cursor_position(void);
+
+
+
 
 void* memset(void* s, int32_t c, uint32_t n);
 void* memset_word(void* s, int32_t c, uint32_t n);
