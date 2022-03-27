02a533c57002e5105e7f3f41309b807a4f4011ec
vrajesh2 <vrajesh2@illinois.edu>
2022-03-27 15:26:02 -0500

    add rtc system calls

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -9,3 +9,8 @@
 extern void initialize_RTC(void);
 //RTC interrupt call 
 extern void interrupt_RTC(void);
+//RTC system calls
+extern int32_t open_RTC (const uint8 t* filename);
+extern int32_t read_RTC (int32 t fd, void* buf, int32 t nbytes);  
+extern int32_t write_RTC (int32 t fd, const void* buf, int32 t nbytes);
+extern int32 t close_RTC (int32 t fd);
