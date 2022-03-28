fa1b84891dc5ba6cc4f3cbbde7d36179dd23bd3a
vrajesh2 <vrajesh2@illinois.edu>
2022-03-27 19:25:39 -0500

    Update tests.h

--- student-distrib/tests.h
+++ student-distrib/tests.h
@@ -13,6 +13,13 @@ int read_file_index();
 int read_file_index_invalid();
 int ls_dir_test();
 
+//RTC tests 
+int rtc_test_open() ;
+int rtc_test_close();
+int rtc_test_read_write_invalid_freq() ;
+int rtc_test_read_write_invalid_size();
+int rtc_test_read_write();
+
 void launch_tests();
 
 #endif /* TESTS_H */
