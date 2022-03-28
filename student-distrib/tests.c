9770ab84ace8cf740cb4240434ed0a5aae06fc01
vrajesh2 <vrajesh2@illinois.edu>
2022-03-28 17:18:33 -0500

    Update tests.c

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -505,6 +505,15 @@ int ls_dir_test(){
 }
 
 //RTC TESTS
+/*RTC TEST
+ * 
+ * Description: tests open functionality
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: NONE
+ * Coverage: RTC
+ * Files: rtc.c/rtc.h
+ */
 int rtc_test_open() {
 	putc('\n');
 	TEST_HEADER;
@@ -512,6 +521,15 @@ int rtc_test_open() {
 	if (test) return FAIL;
 	return PASS;
 }
+/*RTC TEST
+ * 
+ * Description: tests close functionality
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: NONE
+ * Coverage: RTC
+ * Files: rtc.c/rtc.h
+ */
 int rtc_test_close() {
 	putc('\n');
 	TEST_HEADER;
@@ -519,6 +537,15 @@ int rtc_test_close() {
 	if (test) return FAIL;
 	return PASS;
 }
+/*RTC TEST
+ * 
+ * Description: tests write functionality
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: NONE
+ * Coverage: RTC
+ * Files: rtc.c/rtc.h
+ */
 int rtc_test_read_write_invalid_freq() {
 	putc('\n');
 	TEST_HEADER;
@@ -526,6 +553,15 @@ int rtc_test_read_write_invalid_freq() {
 	if (write_RTC(0, &freq, 4) == -1)  return FAIL; //4 is number of bytes in proper size argument 
 	return PASS;
 }
+/*RTC TEST
+ * 
+ * Description: tests write functionality
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: NONE
+ * Coverage: RTC
+ * Files: rtc.c/rtc.h
+ */
 int rtc_test_read_write_invalid_size() {
 	putc('\n');
 	TEST_HEADER;
@@ -533,7 +569,15 @@ int rtc_test_read_write_invalid_size() {
 	if (write_RTC(0, &freq, 3) == -1)  return FAIL;//3 is number of bytes in improper size argument 
 	return PASS;
 }
-
+/*RTC TEST
+ * 
+ * Description: tests read/write functionality
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: NONE
+ * Coverage: RTC
+ * Files: rtc.c/rtc.h
+ */
 int rtc_test_read_write() {
 	TEST_HEADER;
 	uint32_t freq, j;
