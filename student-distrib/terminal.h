ad5859135405a2627477824a006deaed2f1bcf30
dhyey2 <dhyey2@illinois.edu>
2022-03-28 12:37:19 -0500

    adding buffer size and num cols define

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -7,3 +7,7 @@ extern int terminal_close(void);
 extern int terminal_read(int n, unsigned char * buf); //will add fd chkpt3
 
 extern int terminal_write(int n, unsigned char * buf); //will add fd chkpt3
+
+#define keyboardBufferSize 128  //size of the keyboardBuffer 
+#define NUM_COLS    80
+
