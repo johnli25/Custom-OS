c4b15e62c225036847c484c019aa8a831fd93565
dhyey2 <dhyey2@illinois.edu>
2022-03-30 16:54:31 -0500

    adding pcb_t struct

--- student-distrib/systemCalls.h
+++ student-distrib/systemCalls.h
@@ -1,2 +1,12 @@
 int32_t execute (const uint8_t* command);
 
+typedef struct pcb_t{
+    
+    unsigned int pid;
+    unsigned int parent_id;
+    unsigned int fileDescriptor;
+    unsigned int saved_esp;
+    unsigned int saved_ebp;
+    unsigned int active;
+}
+
