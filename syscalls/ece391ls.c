cce64daae478f8ce7fe5af6ffacd2c34e1e50e08
John Li <warriorjohn36@gmail.com>
2022-04-15 18:02:02 -0500

    cringe

--- syscalls/ece391ls.c
+++ syscalls/ece391ls.c
@@ -12,23 +12,17 @@ int main ()
 
     if (-1 == (fd = ece391_open ((uint8_t*)"."))) {
         ece391_fdputs (1, (uint8_t*)"directory open failed\n");
-        ece391_fdputs (1, (uint8_t*)"one\n");
         return 2;
     }
 
     while (0 != (cnt = ece391_read (fd, buf, SBUFSIZE-1))) {
-        ece391_fdputs (1, (uint8_t*)"two\n");
-
         if (-1 == cnt) {
 	        ece391_fdputs (1, (uint8_t*)"directory entry read failed\n");
-            ece391_fdputs (1, (uint8_t*)"trhee\n");
-            return 3;
+	        return 3;
 	    }
 	    buf[cnt] = '\n';
-	    if (-1 == ece391_write (1, buf, cnt + 1)){
-            ece391_fdputs (1, (uint8_t*)"return 3\n");
+	    if (-1 == ece391_write (1, buf, cnt + 1))
 	        return 3;
-        }
     }
 
     return 0;
