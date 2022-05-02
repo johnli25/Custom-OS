ae5ced24c8f46ab9be0c3332fa83c9d1875aa587
John Li <warriorjohn36@gmail.com>
2022-05-01 21:40:48 -0500

    did I just fix rtc virtualization?

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -40,10 +40,10 @@ void initialize_RTC(void){
 void interrupt_RTC(void){
 
     if (multi_terms[currTerm].relative_frequency==1)
-    multi_terms[currTerm].relative_frequency = 5000; 
+        multi_terms[currTerm].relative_frequency = 5000; 
 
     multi_terms[currTerm].rtc_counter++;
-    if (multi_terms[currTerm].rtc_counter >= multi_terms[currTerm].relative_frequency/2) {
+    if (multi_terms[currTerm].rtc_counter == multi_terms[currTerm].relative_frequency/2) {
         state_data[0] = 1; 
         multi_terms[currTerm].rtc_counter = 0; 
     }
