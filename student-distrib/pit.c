40c4a2f1eb97ecb9cafd943dd412b463bd482a87
nmohiu6 <nmohiu6@illinois.edu>
2022-05-02 00:06:29 -0500

    comments

--- student-distrib/pit.c
+++ student-distrib/pit.c
@@ -9,13 +9,27 @@
 https://wiki.osdev.org/Pit#Operating_Modes */
 static int pit_count = 0;
 
+/* initialize_PIT
+ * Description: Initializes PIT
+ * Inputs: None
+ * Outputs: None
+ * Return Value: None
+ * Side Effects: Initializes the PIT by setting the correct BYTES 
+*/ 
 void initialize_PIT(void){
     outb(CMD_BYTE, IO_PORT_NUM);             /* Set our command byte 0x36 */
     outb(PIT_FREQ & 0xFF, CHANNEL_0);   /* Set low byte of divisor */
-    outb((PIT_FREQ /*& 0xFF00*/)>> 8, CHANNEL_0);     /* Set high byte of divisor */
+    outb((PIT_FREQ /*& 0xFF00*/)>> 8, CHANNEL_0); //MAGIC NUM 8: Needed to shift by 8 to set High Bit of Divisor - OSDEV
     enable_irq(PIT_IRQ_NUM);
 }   
 
+/* interrupt_PIT
+ * Description: Creates the interrupt handler for the PIT
+ * Inputs: None
+ * Outputs: None
+ * Return Value: None
+ * Side Effects: Checks the flags and switches to the correct corresponding Terminal
+*/ 
 void interrupt_PIT(void){
     send_eoi(PIT_IRQ_NUM);
     pit_count++;
@@ -33,17 +47,17 @@ void interrupt_PIT(void){
     //     }
     //     break;
     case 1:
-        if (multi_terms[1].bootup_flag == 0){
+        if (multi_terms[1].bootup_flag == 0){ //checks if the flag is 0
             //currTerm = 1;
-            switch_terms(1);
+            switch_terms(1); //MAGIC: switches to the FIRST terminal
             execute((uint8_t*)"shell");
-            multi_terms[1].bootup_flag = 1;
+            multi_terms[1].bootup_flag = 1; //marks the flag as present
         }
         break;
     case 2:
-        if (multi_terms[2].bootup_flag == 0){
+        if (multi_terms[2].bootup_flag == 0){ //checks if the flag is 0
             //currTerm = 2;          
-            switch_terms(2);
+            switch_terms(2); //MAGIC: switches to the SECOND terminal
             execute((uint8_t*)"shell");
 
             /*context switch for terminal switch*/
@@ -57,15 +71,15 @@ void interrupt_PIT(void){
             // multi_terms[currTerm].curr_proc = mypcb;
             // contextSwitch(mypcb, nextpcb);
             /*end of context switch for terminals*/
-            multi_terms[2].bootup_flag = 1;
+            multi_terms[2].bootup_flag = 1; //sets flag as present
         }
         break;
     case 3:
-        if (multi_terms[0].bootup_flag == 0){
+        if (multi_terms[0].bootup_flag == 0){ //checks if flag is present
             //currTerm = 0;
-            switch_terms(0);
+            switch_terms(0); //MAGIC: switches to ZEROth (THIRD) terminal
             execute((uint8_t*)"shell");
-            multi_terms[0].bootup_flag = 1;
+            multi_terms[0].bootup_flag = 1; //marks flag as present 
         }
         break;
     default:
